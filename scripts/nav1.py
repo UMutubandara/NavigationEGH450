741388ec30b62d42f42a52c6f0b104046422a3ef
UMutubandara <umesh.mutubandara@gmail.com>
2017-09-18 21:35:28 +1000

    updatedNav

--- scripts/nav1.py
+++ scripts/nav1.py
@@ -4,13 +4,19 @@ import numpy as np
 import math
 import time
 from geometry_msgs.msg import PoseStamped
-
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Int8
+import std_msgs.msg
 
 class Navigation():
 
 	def __init__(self):
 		#set up publisher 
+		self.msg_out= PoseStamped()
+		self.msg_out.header.frame_id = "world"
+		self.currentwp = Pose()
 		self.pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size =10)
+		
 		self.timer = rospy.Timer(rospy.Duration(0.1), self.pub_callback)
 
 		#set up test waypoints and waypoint counter
@@ -22,25 +28,21 @@ class Navigation():
 						[-2 ,-2 ,2 ],
 						[0 ,0 ,0 ]]
 		self.waypoint_counter = 0
+		self.land = 0
 
 		# Set up the subscriber
 		self.sub_ping = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.callback)
+		self.sub_land = rospy.Subscriber("/land", Int8, self.ground)
 
 	def pub_callback(self, event):
 		#print 'Timer called at ' + str(event.current_real)
-		msg_out= PoseStamped()
+		self.msg_out.header.stamp = event.current_real
+
 		#fill in goal msg
-		msg_out.pose.position.x = self.waypoint[self.waypoint_counter][0]
-		msg_out.pose.position.y = self.waypoint[self.waypoint_counter][1]
-		msg_out.pose.position.z = self.waypoint[self.waypoint_counter][2]
-		msg_out.pose.orientation.x = 0
-		msg_out.pose.orientation.y = 0
-		msg_out.pose.orientation.z = 1
-		msg_out.pose.orientation.w = 1
-
-
-		self.pub.publish(msg_out)
-
+		self.msg_out.pose= self.currentwp
+			
+		self.pub.publish(self.msg_out)
+		rospy.loginfo("NextPoint Position: [ %f, %f, %f ]"%(self.msg_out.pose.position.x, self.msg_out.pose.position.y, self.msg_out.pose.position.z))
 
 	def shutdown(self):
 		# Unregister anything that needs it here
@@ -52,18 +54,39 @@ class Navigation():
 		self.uav_pose = msg.pose
 
 		
+		self.near_waypoint = 0.10
+
 		self.distanceto = np.sqrt(((self.uav_pose.position.x - self.waypoint[self.waypoint_counter][0])**2)+((self.uav_pose.position.y - 
 		self.waypoint[self.waypoint_counter][1])**2)+((self.uav_pose.position.z - self.waypoint[self.waypoint_counter][2])**2))
 
-		rospy.loginfo(self.distanceto)
-
-		self.near_waypoint = 0.2
-
-
-		if (self.distanceto < self.near_waypoint) and (self.waypoint_counter < 6):
+		if ((self.distanceto < self.near_waypoint) and (self.waypoint_counter < 6)):
 		 	self.waypoint_counter += 1
-		 	time.sleep(5)
+		 	self.currentwp.position.x = self.waypoint[self.waypoint_counter][0]
+			self.currentwp.position.y = self.waypoint[self.waypoint_counter][1]
+			self.currentwp.position.z = self.waypoint[self.waypoint_counter][2]
+			self.currentwp.orientation.x = 0
+			self.currentwp.orientation.y = 0
+			self.currentwp.orientation.z = 1
+			self.currentwp.orientation.w = 1
 
+
+	
+	def ground(self, msg):
+		self.eground = msg.data
+		#rospy.loginfo(msg.data)
+		if (self.eground > 2):
+			self.currentwp.position.x = 0#self.uav_pose.position.x
+			self.currentwp.position.y = 0#self.uav_pose.position.y
+			self.currentwp.position.z = 3
+
+		else:
+			self.currentwp.position.x = self.waypoint[self.waypoint_counter][0]
+			self.currentwp.position.y = self.waypoint[self.waypoint_counter][1]
+			self.currentwp.position.z = self.waypoint[self.waypoint_counter][2]
+			self.currentwp.orientation.x = 0
+			self.currentwp.orientation.y = 0
+			self.currentwp.orientation.z = 1
+			self.currentwp.orientation.w = 1
 
 
 if __name__ == '__main__':
